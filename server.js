e0c68fe707478622aa8cb6a8ad61227da28fd751
Tal Elias <talelias2@gmail.com>
2023-10-02 00:32:58 +0300

    fixed dotenv config

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import cookieParser from 'cookie-parser'
 import cors  from 'cors'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
